db99a72e76caf6c9f367dc36a7ced0324333fbe9
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-30 10:02:36 +0100

    database

---
+++ src/util/data.js
@@ -0,0 +1,116 @@
+import apple from "../assets/product1.png";
+import home from "../assets/product2.png";
+import cabbage from "../assets/product3.png";
+import capicum from "../assets/product4.png";
+
+import vegetable from "../assets/category1.png";
+import fruth from "../assets/category2.png";
+import fish from "../assets/category3.png";
+import meat from "../assets/category4.png";
+import drinks from "../assets/category5.png";
+import snacks from "../assets/category6.png";
+export const LANGUAGES = [
+  { code: "en", name: "English" },
+  { code: "es", name: "Spanish" },
+  { code: "fr", name: "French" },
+  { code: "de", name: "German" },
+  { code: "zh", name: "Chinese" },
+  { code: "ja", name: "Japanese" },
+  { code: "ru", name: "Russian" },
+  { code: "pt", name: "Portuguese" },
+  { code: "it", name: "Italian" },
+  { code: "nl", name: "Dutch" },
+  { code: "ko", name: "Korean" },
+  { code: "ar", name: "Arabic" },
+  { code: "hi", name: "Hindi" },
+  { code: "el", name: "Greek" },
+  { code: "he", name: "Hebrew" },
+  { code: "sv", name: "Swedish" },
+  { code: "tr", name: "Turkish" },
+  { code: "da", name: "Danish" },
+  { code: "pl", name: "Polish" },
+  { code: "th", name: "Thai" },
+];
+
+export const CURRENCIES = [
+  { code: "USD", name: "United States Dollar" },
+  { code: "EUR", name: "Euro" },
+  { code: "JPY", name: "Japanese Yen" },
+  { code: "GBP", name: "British Pound Sterling" },
+  { code: "AUD", name: "Australian Dollar" },
+  { code: "CAD", name: "Canadian Dollar" },
+  { code: "CHF", name: "Swiss Franc" },
+  { code: "CNY", name: "Chinese Yuan" },
+  { code: "SEK", name: "Swedish Krona" },
+  { code: "NZD", name: "New Zealand Dollar" },
+  { code: "MXN", name: "Mexican Peso" },
+  { code: "SGD", name: "Singapore Dollar" },
+  { code: "HKD", name: "Hong Kong Dollar" },
+  { code: "NOK", name: "Norwegian Krone" },
+  { code: "KRW", name: "South Korean Won" },
+  { code: "TRY", name: "Turkish Lira" },
+  { code: "INR", name: "Indian Rupee" },
+  { code: "RUB", name: "Russian Ruble" },
+  { code: "BRL", name: "Brazilian Real" },
+  { code: "ZAR", name: "South African Rand" },
+];
+
+export const PRODUCT = [
+  {
+    image: apple,
+    name: "Green Apple",
+    price: 14.99,
+    discount: 50,
+  },
+  {
+    image: home,
+    name: "Chanise Cabbage",
+    price: 14.99,
+    discount: 0,
+  },
+  {
+    image: cabbage,
+    name: "Green Capsicum",
+    price: 14.99,
+    discount: 0,
+  },
+  {
+    image: capicum,
+    name: "Ladies Finger",
+    price: 14.99,
+    discount: 0,
+  },
+];
+
+export const PRODUCT_CATEGORY = [
+  {
+    image: vegetable,
+    title: "Vegetables",
+    text: 165,
+  },
+  {
+    image: fruth,
+    title: "Fresh Fruth",
+    text: 137,
+  },
+  {
+    image: fish,
+    title: "Fish",
+    text: 34,
+  },
+  {
+    image: meat,
+    title: "Meat",
+    text: 165,
+  },
+  {
+    image: drinks,
+    title: "Drinks",
+    text: 34,
+  },
+  {
+    image: snacks,
+    title: "Snacks",
+    text: 165,
+  },
+];
