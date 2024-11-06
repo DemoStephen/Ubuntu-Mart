10b3ed0949458ac13155a3cf1566816801cb5e10
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-11-06 21:42:04 +0100

    updated Products functions

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -6,7 +6,15 @@ import ProductDetail from "./ProductDetail";
 
 export default function Product(props) {
   return (
-    <div className="relative border-2 p-4 rounded-lg hover:shadow-lg hover:shadow-green-100  hover:border-green-400 cursor-pointer w-full overflow-hidden group">
+    <div className="relative border-2 p-4 rounded-lg hover:shadow-lg hover:shadow-green-100 hover:border-green-400 cursor-pointer w-full overflow-hidden group">
+      <p
+        className={`animate-pulse absolute left-2 top-2  px-2 py-1 rounded-lg text-white ${
+          props.discount > 10 ? "bg-red-600" : "bg-orange-500"
+        }`}
+      >
+        {props.discount}% Discount
+      </p>
+
       <AddToFavorite />
       <img src={props.image} alt={props.name} />
       <div className="flex items-center justify-between">
--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable react/prop-types */
+import AddToCartButton from "./AddToCartButton";
+import AddToFavorite from "./AddtoFavoriteButton";
 import Rating from "./Rating";
 
 export default function ProductDetail({ ...props }) {
@@ -9,16 +11,34 @@ export default function ProductDetail({ ...props }) {
   }
   return (
     <div>
-      <p>{props.name}</p>
-      <p className="flex gap-4 text-xl -mt-1">
-        <span>${newPrice.toFixed(2)}</span>
-        {props.discount > 0 && props.discount && (
-          <span className="text-slate-500 line-through">
-            {props.price.toFixed(2)}
-          </span>
-        )}
+      <p className="group-hover:text-green-500  font-semibold">
+        {props.name || "Error Name"}
       </p>
-      <Rating />
+      <div
+        className={
+          props.specialDeal ? "p-1 h-11 group-hover:hidden" : "p-1 h-11"
+        }
+      >
+        <p className="flex gap-4 text-xl -mt-1">
+          <span>
+            ${isNaN(newPrice.toFixed(2)) ? "Error Price" : newPrice.toFixed(2)}
+          </span>
+          {props.discount > 0 && props.discount && (
+            <span className="text-slate-500 line-through">
+              {isNaN(props.price.toFixed(2))
+                ? "Error Discount"
+                : props.price.toFixed(2)}
+            </span>
+          )}
+        </p>
+        <Rating />
+      </div>
+      {props.specialDeal && (
+        <div className="hidden gap-2 h-11 group-hover:flex">
+          <AddToCartButton deals />
+          <AddToFavorite deals />
+        </div>
+      )}
     </div>
   );
 }
