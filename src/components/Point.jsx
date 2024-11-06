7ab7512b1f97ac823980da3a4cd086df7fab0b0f
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-11-06 13:18:32 +0100

    Updated Points

--- src/components/Point.jsx
+++ src/components/Point.jsx
@@ -1,21 +1,21 @@
 /* eslint-disable react/prop-types */
-export default function Point({ image, title, text, number }) {
+export default function Point({ image, name, text, number }) {
   let classNames =
-    "flex md:flex-col items-center gap-4 md:text-center p-2 md:p-8 w-full cursor-pointer";
+    "flex md:flex-col items-center gap-4 md:text-center p-2 md:p-8 w-full cursor-pointer group";
   if (!number) {
     classNames +=
       " border-b-4 md:border-transparent md:hover:border-green-600 duration-300";
   } else {
     classNames +=
-      " rounded-lg hover:shadow-lg hover:shadow-green-100 flex-col hover:border-green-400 md:border-2 text-center bg-white";
+      " rounded-lg hover:shadow-lg hover:shadow-green-100 flex-col hover:border-green-500 md:border-2 text-center bg-white";
   }
   return (
     <div className={classNames}>
       <div>
-        <img src={image} alt={title} className="md:block m-auto w-16" />
+        <img src={image} alt={name} className="md:block m-auto w-16" />
       </div>
       <div>
-        <h4 className="font-semibold">{title}</h4>
+        <h4 className="font-bold group-hover:text-green-600">{name}</h4>
         <p className="text-sm">
           {text} {number && "Products"}
         </p>
--- src/components/Point2.jsx
+++ src/components/Point2.jsx
@@ -1,8 +1,23 @@
+// import { useState, useEffect } from "react";
+
 /* eslint-disable react/prop-types */
-export default function Point2({ number, text }) {
+export default function Point2({ number, text, children }) {
+  // const [value, setValue] = useState(0);
+  // useEffect(() => {
+  //   if (value === number) return;
+  //   const intervalId = setInterval(() => {
+  //     setValue((prev) => prev + 1);
+  //   }, 2);
+  //   return () => clearInterval(intervalId);
+  // }, [value]);
+
   return (
     <div className="backdrop-brightness-200 px-14 py-8 text-center rounded-lg w-full lg:w-1/4 md:w-full">
-      <h4 className="text-5xl mb-4 text-orange-500">{number}</h4>
+      <h4 className="text-5xl mb-4 text-orange-500">
+        {/* {value} */}
+        {number}
+        {children}
+      </h4>
       <p className="text-white">{text}</p>
     </div>
   );
