b59f09f2866aa551a1ed85728438445e303b2a28
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-11-07 17:02:59 +0100

    fixed button bug

--- src/components/ButtonShopNow.jsx
+++ src/components/ButtonShopNow.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable react/prop-types */
 import Icon from "./Icon";
 
-export default function ButtonShopNow(color) {
+export default function ButtonShopNow({ color }) {
   return (
     <button
-      className={`flex items-center gap-2 py-2 px-8 rounded-full ${
+      className={`flex items-center gap-2 py-2 px-8 rounded-full mt-4 ${
         color ? "bg-slate-50 text-orange-500" : "bg-orange-500 text-slate-50"
       }`}
     >
