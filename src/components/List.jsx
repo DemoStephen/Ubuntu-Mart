7f7c67daf48b9720ab7a49a5b04c6746dbdc5dcd
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-11-07 19:22:29 +0100

    added an anchor tag to the list items

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,4 +1,8 @@
 // eslint-disable-next-line react/prop-types
-export default function List({ name }) {
-  return <li className="px-2 cursor-pointer hover:text-slate-700">{name}</li>;
+export default function List({ name, href }) {
+  return (
+    <a href={href}>
+      <li className="px-2 cursor-pointer hover:text-slate-700">{name}</li>
+    </a>
+  );
 }
