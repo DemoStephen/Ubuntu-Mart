2ed487432eaefe86bc788bdf75167bc2886d1672
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-11-07 19:21:45 +0100

    new features I guess

--- src/UI/Deal.jsx
+++ src/UI/Deal.jsx
@@ -3,8 +3,8 @@ import ProductDetail from "../components/ProductDetail";
 
 export default function Deal({ ...props }) {
   return (
-    <article className="group flex items-center justify-between px-4 py-2 border-2 border-green-400 hover:shadow-lg hover:shadow-green-100  hover:border-green-600 rounded-lg cursor-pointer">
-      <img src={props.image} alt="" className="w-20" />
+    <article className="group flex items-center gap-6 justify-between px-4 py-2 border-2 border-green-400 hover:shadow-lg hover:shadow-green-100  hover:border-green-600 rounded-lg cursor-pointer">
+      <img src={props.image} alt="" className="w-1/4" />
       <ProductDetail {...props} specialDeal />
     </article>
   );
--- src/UI/TopCategories.jsx
+++ src/UI/TopCategories.jsx
@@ -5,7 +5,10 @@ import leave from "../assets/images/Vector/vector_assets7.png";
 import green from "../assets/images/Vector/vector_assets9.png";
 export default function TopCategories() {
   return (
-    <section className="px-[10%] py-16 bg-gradient-to-b from-green-50 to-gray-50 relative">
+    <section
+      className="px-[10%] py-16 bg-gradient-to-b from-green-50 to-gray-50 relative"
+      id="shop"
+    >
       <SectionHeader title="Shop By Top Categories" />
       <section className="grid grid-cols-2 md:grid-cols-3 lg:flex items-center gap-4 mt-8">
         {CATEGORIES.map((category) => {
