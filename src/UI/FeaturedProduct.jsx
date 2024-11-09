b3546e7b61f6cdd18d7440af68104a99a380ae87
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-11-09 23:32:49 +0100

    updated products section

--- src/UI/FeaturedProduct.jsx
+++ src/UI/FeaturedProduct.jsx
@@ -25,7 +25,7 @@ export default function FeaturedProduct() {
   return (
     <section className="px-[5%] md:px-[10%] py-12 relative overflow-hidden">
       <SectionHeader title="Featured Product" />
-      <section className="flex flex-col md:grid md:grid-cols-2 lg:flex lg:flex-row items-center justify-between gap-6 mt-8">
+      <section className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 xl:flex xl:flex-row items-center justify-between gap-6 mt-8">
         {randomProduct.map((product) => (
           <Product key={product.name} {...product} />
         ))}
