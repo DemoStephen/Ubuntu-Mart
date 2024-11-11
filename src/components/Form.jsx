1b8610cffd743dfc172c20f39c904b5378c886a5
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-11-11 13:02:20 +0100

    form section updated

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,20 +1,25 @@
-export default function Form() {
+/* eslint-disable react/prop-types */
+export default function Form({ color }) {
+  let classList = "p-2 rounded-full md:w-1/3 text-white";
+  if (color) {
+    classList += " bg-green-500 ";
+  } else {
+    classList += " bg-orange-500 ";
+  }
   return (
     <>
       <form
         action="#"
-        className="flex flex-col md:flex-row md:border-2 border-orange-500 outline-none md:rounded-full overflow-hidden p-1 gap-2"
+        className="flex flex-col md:flex-row md:border-2 border-slate-200 bg-slate-50 outline-none rounded-3xl md:rounded-full overflow-hidden p-1 gap-2 w-full"
       >
         <input
           type="email"
           name="email"
           id="email"
           placeholder="Your email address Subscribe"
-          className="p-3 w-full outline-none "
+          className="p-2 w-full outline-none text-center md:text-left"
         />
-        <button className="p-3 bg-orange-500 rounded-full md:w-1/3 text-white">
-          Subscribe
-        </button>
+        <button className={classList}>Subscribe</button>
       </form>
     </>
   );
