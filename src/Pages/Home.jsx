59993d7b792431e49874c42a7eaa639eb75e8305
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-11-11 13:05:41 +0100

    � working on the footer section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,11 +3,13 @@ import Blogs from "../UI/Blogs";
 import Deals from "../UI/Deals";
 import DiscountSales from "../UI/DiscountSales";
 import FeaturedProduct from "../UI/FeaturedProduct";
+import Footer from "../UI/Footer";
 import Header from "../UI/Header";
 import Navigation from "../UI/Navigation";
 import Partners from "../UI/Partners";
 import SellingPoint from "../UI/SellingPoint";
 import SellingPoint2 from "../UI/SellingPoint2";
+import Subscribe from "../UI/Subscribe";
 import Testimonial from "../UI/Testimonial";
 import TopCategories from "../UI/TopCategories";
 import Trust from "../UI/Trust";
@@ -28,6 +30,8 @@ export default function Home() {
       <Blogs />
       <Testimonial />
       <Partners />
+      <Subscribe />
+      <Footer />
     </>
   );
 }
---
+++ src/UI/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="px-[10%] py-8 bg-blue-950 text-slate-50 text-center text-2xl">
+      Stephen is currently working on the footer. Come back okay
+    </footer>
+  );
+}
