9750ad032bfc481e9167fec232c1576c9555c740
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-11-11 09:19:59 +0100

    partners updated

--- src/UI/Partners.jsx
+++ src/UI/Partners.jsx
@@ -1,10 +1,21 @@
-import company from "../assets/images/images/partner.png";
+import partner1 from "../assets/images/logo/bookoff.png";
+import partner2 from "../assets/images/logo/food.png";
+import partner3 from "../assets/images/logo/food2.png";
+import partner4 from "../assets/images/logo/G series.png";
+import partner5 from "../assets/images/logo/steps.png";
+import partner6 from "../assets/images/logo/mango.png";
+
 import Form from "../components/Form";
 export default function Partners() {
   return (
     <section className="px-[10%] py-10">
-      <div className="full flex items-center justify-between">
-        <img src={company} alt="" className="w-full" />
+      <div className="full grid grid-cols-3 md:grid-cols-6 items-center justify-center gap-2 md:gap-8">
+        <img src={partner1} alt="partner1" className="w-3/4" />
+        <img src={partner2} alt="partner2" className="w-3/4" />
+        <img src={partner3} alt="partner3" className="w-3/4" />
+        <img src={partner4} alt="partner4" className="w-3/4" />
+        <img src={partner5} alt="partner5" className="w-3/4" />
+        <img src={partner6} alt="partner1" className="w-3/4" />
       </div>
       <section className="flex flex-col md:grid md:grid-cols-4 gap-8">
         <div className="p-4 shadow-md">
