8c19ba1dca3d633b7d4084715b11e4c362f3917b
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-11-20 15:12:20 +0100

    Updated the SVGs Compoents

--- src/components/SVGs.jsx
+++ src/components/SVGs.jsx
@@ -15,25 +15,6 @@ export function ArrowRight() {
   );
 }
 
-export function LoveIcon() {
-  return (
-    <Icon>
-      <path
-        d="M12 5.50063C7.50016 0.825464 2 4.27416 2 9.1371C2 14 6.01943 16.5914 8.96173 18.9109C10 19.7294 11 20.5 12 20.5"
-        stroke="#000"
-        strokeWidth="1"
-        strokeLinecap="round"
-      />
-      <path
-        d="M12 5.50063C16.4998 0.825464 22 4.27416 22 9.1371C22 14 17.9806 16.5914 15.0383 18.9109C14 19.7294 13 20.5 12 20.5"
-        stroke="#000"
-        strokeWidth="1"
-        strokeLinecap="round"
-      />
-    </Icon>
-  );
-}
-
 export function ViewIcon() {
   return (
     <Icon>
@@ -338,6 +319,25 @@ export function Bag() {
           strokeLinecap="round"
         />
       </>
+    </Icon>
+  );
+}
+
+export function LoveIcon() {
+  return (
+    <Icon>
+      <path
+        d="M12 5.50063C7.50016 0.825464 2 4.27416 2 9.1371C2 14 6.01943 16.5914 8.96173 18.9109C10 19.7294 11 20.5 12 20.5"
+        stroke="#000"
+        strokeWidth="1"
+        strokeLinecap="round"
+      />
+      <path
+        d="M12 5.50063C16.4998 0.825464 22 4.27416 22 9.1371C22 14 17.9806 16.5914 15.0383 18.9109C14 19.7294 13 20.5 12 20.5"
+        stroke="#000"
+        strokeWidth="1"
+        strokeLinecap="round"
+      />
     </Icon>
   );
 }
