d6603363912d4187997a87ca12c15d33323eca85
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-11-20 15:14:10 +0100

    working on update for the about us page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+// import AboutUs from "./Pages/AboutUs";
 import Home from "./Pages/Home";
 
 export default function App() {
   return (
     <>
+      {/* <AboutUs /> */}
       <Home />
     </>
   );
