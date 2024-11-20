2c836f1471a559e2258e95221ff39423004eb4d6
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-11-20 15:10:52 +0100

    working on the about us page.

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,5 +1,31 @@
+import Location from "../components/Location";
+import { Bag, LoveIcon } from "../components/SVGs";
+import Search from "../UI/Search";
+import SubNavigation from "../UI/SubNavigation";
+
 export default function AboutUs() {
-  <>
-    <h1>About Us</h1>
-  </>;
+  return (
+    <>
+      <nav>
+        <Location AboutUs />
+        <div className="lg:px-[10%] px-[5%] py-3 border flex items-center md:grid md:grid-cols-2 lg:grid-cols-3 gap-4 justify-between">
+          <div className="lg:col-start-2">
+            <Search />
+          </div>
+          <div className="flex items-center gap-2 lg:gap-4 justify-end">
+            <LoveIcon />
+            <hr className="h-full border py-3" />
+            <div className="flex items-center justify-center gap-2">
+              <Bag />
+              <p className="hidden md:flex flex-col">
+                <span className="text-xs">Shopping Cart</span>
+                <b className="text-sm">$57.00</b>
+              </p>
+            </div>
+          </div>
+        </div>
+        <SubNavigation AboutUs />
+      </nav>
+    </>
+  );
 }
